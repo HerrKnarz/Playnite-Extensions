3725d94c2cfb1aa1758b9d3a659b8b1d1a0ce206
HerrKnarz <40741234+HerrKnarz@users.noreply.github.com>
2022-11-14 17:14:17 +0100

    Added support for Co-Optimus (search only)

---
+++ Source/Linker/LinkSources/LinkCoOptimus.cs
@@ -0,0 +1,56 @@
+﻿using HtmlAgilityPack;
+using LinkUtilities.Helper;
+using LinkUtilities.Models;
+using Playnite.SDK;
+using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace LinkUtilities.Linker
+{
+    /// <summary>
+    /// Adds a link to Co-Optimus.
+    /// </summary>
+    class LinkCoOptimus : Link
+    {
+        public override string LinkName { get; } = "Co-Optimus";
+        public override string BaseUrl { get; } = "";
+        public override string SearchUrl { get; } = "https://api.co-optimus.com/games.php?search=true&name=";
+
+        public override List<GenericItemOption> SearchLink(string searchTerm)
+        {
+            SearchResults.Clear();
+
+            try
+            {
+                HtmlWeb web = new HtmlWeb();
+                HtmlDocument doc = web.Load($"{SearchUrl}{searchTerm.UrlEncode()}");
+
+                HtmlNodeCollection htmlNodes = doc.DocumentNode.SelectNodes("//game");
+
+                if (htmlNodes != null && htmlNodes.Count > 0)
+                {
+                    foreach (HtmlNode node in htmlNodes)
+                    {
+                        SearchResults.Add(new SearchResult
+                        {
+                            Name = WebUtility.HtmlDecode(node.SelectSingleNode("./title").InnerText),
+                            Url = node.SelectSingleNode("./url").InnerText,
+                            Description = $"{WebUtility.HtmlDecode(node.SelectSingleNode("./system").InnerText)} - {WebUtility.HtmlDecode(node.SelectSingleNode("./publisher").InnerText)} ({node.SelectSingleNode("./releasedate").InnerText})"
+                        });
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, $"Error loading data from {LinkName}");
+            }
+
+            return base.SearchLink(searchTerm);
+        }
+
+        public LinkCoOptimus(LinkUtilities plugin) : base(plugin)
+        {
+        }
+    }
+}
--- Source/Linker/Links.cs
+++ Source/Linker/Links.cs
@@ -9,6 +9,7 @@ namespace LinkUtilities.Linker
     {
         public Links(LinkUtilities plugin)
         {
+            Add(new LinkCoOptimus(plugin));
             Add(new LinkEpic(plugin));
             Add(new LibraryLinkGog(plugin));
             Add(new LinkHG101(plugin));
