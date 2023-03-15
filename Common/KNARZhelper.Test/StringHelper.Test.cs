3f9213898771e95f4f92cd0b4dbf58e7205da7c4
HerrKnarz <40741234+HerrKnarz@users.noreply.github.com>
2023-03-15 14:09:24 +0100

    Completed Tests for StringHelper

--- Common/KNARZhelper.Test/StringHelper.Test.cs
+++ Common/KNARZhelper.Test/StringHelper.Test.cs
@@ -6,26 +6,67 @@ namespace KNARZhelper.Test
     {
         [Theory]
         [InlineData("This is a test with äÖüß and other chars.", "This is a test with  and other chars")]
-        [InlineData("Trüberbrook", "Trberbrook")]
-        public void TestRemoveSpecialChars(string stringWithUmlaut, string stringWithoutUmlaut)
+        public void TestRemoveSpecialChars(string stringToCheck, string stringResult)
         {
-            Assert.Equal(stringWithoutUmlaut, stringWithUmlaut.RemoveSpecialChars());
+            Assert.Equal(stringResult, stringToCheck.RemoveSpecialChars());
         }
 
         [Theory]
         [InlineData("This is a test with äÖüß and other chars.", "This is a test with aOuss and other chars.")]
-        [InlineData("Trüberbrook", "Truberbrook")]
-        public void TestRemoveDiactritics(string stringWithUmlaut, string stringWithoutUmlaut)
+        public void TestRemoveDiactritics(string stringToCheck, string stringResult)
         {
-            Assert.Equal(stringWithoutUmlaut, stringWithUmlaut.RemoveDiacritics());
+            Assert.Equal(stringResult, stringToCheck.RemoveDiacritics());
         }
 
         [Theory]
         [InlineData("   This is   a test      .", "This is a test .")]
-        [InlineData("Trüber  brook", "Trüber brook")]
-        public void TestCollapesWhitespaces(string stringWithUmlaut, string stringWithoutUmlaut)
+        public void TestCollapesWhitespaces(string stringToCheck, string stringResult)
         {
-            Assert.Equal(stringWithoutUmlaut, stringWithUmlaut.CollapseWhitespaces());
+            Assert.Equal(stringResult, stringToCheck.CollapseWhitespaces());
+        }
+
+        [Theory]
+        [InlineData("This is a über test.", "This%20is%20a%20%C3%BCber%20test.")]
+        public void TestEscapeDataString(string stringToCheck, string stringResult)
+        {
+            Assert.Equal(stringResult, stringToCheck.EscapeDataString());
+        }
+
+        [Theory]
+        [InlineData("This is a über test.", "This+is+a+%C3%BCber+test.")]
+        public void TestUrlEncode(string stringToCheck, string stringResult)
+        {
+            Assert.Equal(stringResult, stringToCheck.UrlEncode());
+        }
+
+        [Theory]
+        [InlineData("This is a \"test\".", "This is a \\\"test\\\".")]
+        public void TestEscapeQuotes(string stringToCheck, string stringResult)
+        {
+            Assert.Equal(stringResult, stringToCheck.EscapeQuotes());
+        }
+
+        [Theory]
+        [InlineData("This IS a tesT!", "This Is A Test!")]
+        public void TestToTitleCase(string stringToCheck, string stringResult)
+        {
+            Assert.Equal(stringResult, stringToCheck.ToTitleCase());
+        }
+
+        [Theory]
+        [InlineData("This is a test with 1 and 6 and 12.", "This is a test with I and VI and III.")]
+        public void TestDigitsToRomanNumbers(string stringToCheck, string stringResult)
+        {
+            Assert.Equal(stringResult, stringToCheck.DigitsToRomanNumbers());
+        }
+
+        [Theory]
+        [InlineData("This is a (test)", "(", ")", "This is a ")]
+        [InlineData("This (is) a (test)", "(", ")", "This  a ")]
+        [InlineData("This <testis</test> a (test)</test>", "<test", "</test>", "This  a (test)</test>")]
+        public void TestRemoveTextBetween(string stringToCheck, string removeFrom, string removeTo, string stringResult)
+        {
+            Assert.Equal(stringResult, stringToCheck.RemoveTextBetween(removeFrom, removeTo));
         }
     }
 }
--- Common/KNARZhelper/PlatformHelper.cs
+++ Common/KNARZhelper/PlatformHelper.cs
@@ -50,7 +50,7 @@ namespace KNARZhelper
             TryAddPlatformByName(platformSpecNameByNormalName, "Playstation 4/5", new[] { "sony_playstation4", "sony_playstation5" });
             TryAddPlatformByName(platformSpecNameByNormalName, new[] { "Sega Mega Drive", "Mega Drive", "Mega Drive/Genesis" }, new[] { "sega_genesis" });
             TryAddPlatformByName(platformSpecNameByNormalName, new[] { "Super NES", "Super Nintendo Entertainment System" }, new[] { "nintendo_super_nes" });
-            //TryAddPlatformByName(platformSpecNameByNormalName, new[] { "SNK Neo Geo MVS", "Neo Geo MVS" }, new[] { "snk_neo_geo" });
+            TryAddPlatformByName(platformSpecNameByNormalName, new[] { "SNK Neo Geo MVS", "Neo Geo MVS" }, new[] { "snk_neo_geo_aes" });
         }
         private bool TryAddPlatformByName(Dictionary<string, string[]> dict, string platformName, params string[] platformSpecNames)
         {
