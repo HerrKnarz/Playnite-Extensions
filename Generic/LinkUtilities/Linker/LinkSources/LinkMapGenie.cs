56a227b18884bae4b6a0a4478404212a478f4154
HerrKnarz <40741234+HerrKnarz@users.noreply.github.com>
2023-03-24 22:00:39 +0100

    Added short delay, so we don't get blocked for too many requests.

--- Generic/LinkUtilities/Linker/LinkSources/LinkMapGenie.cs
+++ Generic/LinkUtilities/Linker/LinkSources/LinkMapGenie.cs
@@ -1,5 +1,6 @@
 ﻿using KNARZhelper;
 using Playnite.SDK.Models;
+using System.Threading;
 
 namespace LinkUtilities.Linker
 {
@@ -13,12 +14,18 @@ namespace LinkUtilities.Linker
 
         // Map Genie Links need the game name in lowercase without special characters and hyphens instead of white spaces.
         public override string GetGamePath(Game game, string gameName = null)
-            => (gameName ?? game.Name).RemoveDiacritics()
-                .RemoveSpecialChars()
-                .Replace("_", " ")
-                .CollapseWhitespaces()
-                .Replace(" ", "-")
-                .ToLower();
+        {
+            // sleep a while, so the site doesn't block us for too many requests. 
+            // Todo: put the interval into the interface, so all links could use it, and put it in the loop to only use the delay, when adding several
+            // links to the site in bulk.
+            Thread.Sleep(200);
+            return (gameName ?? game.Name).RemoveDiacritics()
+                  .RemoveSpecialChars()
+                  .Replace("_", " ")
+                  .CollapseWhitespaces()
+                  .Replace(" ", "-")
+                  .ToLower();
+        }
 
         public LinkMapGenie(LinkUtilities plugin) : base(plugin)
         {
