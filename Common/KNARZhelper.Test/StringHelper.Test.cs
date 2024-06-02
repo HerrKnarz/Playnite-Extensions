0e2b667bebbed767abda2e7a36e3b17914f1afe4
Alexander Heidn <40741234+HerrKnarz@users.noreply.github.com>
2024-06-02 14:55:13 +0200

    fixed test case

--- Common/KNARZhelper.Test/StringHelper.Test.cs
+++ Common/KNARZhelper.Test/StringHelper.Test.cs
@@ -5,39 +5,9 @@ namespace KNARZhelper.Test
     public class StringHelperTest
     {
         [Theory]
-        [InlineData("This is a test with äÖüß and other chars.", "This is a test with  and other chars")]
-        public void TestRemoveSpecialChars(string stringToCheck, string stringResult)
-            => Assert.Equal(stringResult, stringToCheck.RemoveSpecialChars());
-
-        [Theory]
-        [InlineData("This is a test with äÖüß and other chars.", "This is a test with aOuss and other chars.")]
-        public void TestRemoveDiacritics(string stringToCheck, string stringResult)
-            => Assert.Equal(stringResult, stringToCheck.RemoveDiacritics());
-
-        [Theory]
         [InlineData("   This is   a test      .", "This is a test .")]
         public void TestCollapseWhitespaces(string stringToCheck, string stringResult)
             => Assert.Equal(stringResult, stringToCheck.CollapseWhitespaces());
-
-        [Theory]
-        [InlineData("This is a über test.", "This%20is%20a%20%C3%BCber%20test.")]
-        public void TestEscapeDataString(string stringToCheck, string stringResult)
-            => Assert.Equal(stringResult, stringToCheck.EscapeDataString());
-
-        [Theory]
-        [InlineData("This is a über test.", "This+is+a+%C3%BCber+test.")]
-        public void TestUrlEncode(string stringToCheck, string stringResult)
-            => Assert.Equal(stringResult, stringToCheck.UrlEncode());
-
-        [Theory]
-        [InlineData("This is a \"test\".", "This is a \\\"test\\\".")]
-        public void TestEscapeQuotes(string stringToCheck, string stringResult)
-            => Assert.Equal(stringResult, stringToCheck.EscapeQuotes());
-
-        [Theory]
-        [InlineData("This IS a tesT!", "This Is A Test!")]
-        public void TestToTitleCase(string stringToCheck, string stringResult)
-            => Assert.Equal(stringResult, stringToCheck.ToTitleCase());
 
         [Theory]
         [InlineData("This is a test with 1 and 6 and 12.", "This is a test with I and VI and III.")]
@@ -45,11 +15,19 @@ namespace KNARZhelper.Test
             => Assert.Equal(stringResult, stringToCheck.DigitsToRomanNumbers());
 
         [Theory]
-        [InlineData("This is a (test)", "(", ")", "This is a ")]
-        [InlineData("This (is) a (test)", "(", ")", "This  a ")]
-        [InlineData("This <testis</test> a (test)</test>", "<test", "</test>", "This  a (test)</test>")]
-        public void TestRemoveTextBetween(string stringToCheck, string removeFrom, string removeTo, string stringResult)
-            => Assert.Equal(stringResult, stringToCheck.RemoveTextBetween(removeFrom, removeTo));
+        [InlineData("This is a über test.", "This%20is%20a%20%C3%BCber%20test.")]
+        public void TestEscapeDataString(string stringToCheck, string stringResult)
+            => Assert.Equal(stringResult, stringToCheck.EscapeDataString());
+
+        [Theory]
+        [InlineData("This is a \"test\".", "This is a \\\"test\\\".")]
+        public void TestEscapeQuotes(string stringToCheck, string stringResult)
+            => Assert.Equal(stringResult, stringToCheck.EscapeQuotes());
+
+        [Theory]
+        [InlineData("This is a test with äÖüß and other chars.", "This is a test with aOuss and other chars.")]
+        public void TestRemoveDiacritics(string stringToCheck, string stringResult)
+            => Assert.Equal(stringResult, stringToCheck.RemoveDiacritics());
 
         [Theory]
         [InlineData("Doom HD", "Doom")]
@@ -62,5 +40,27 @@ namespace KNARZhelper.Test
         [InlineData("Just a normal game", "Just a normal game")]
         public void TestRemoveEditionSuffix(string stringToCheck, string stringResult)
             => Assert.Equal(stringResult, stringToCheck.RemoveEditionSuffix());
+
+        [Theory]
+        [InlineData("This is a test with äÖüß and other chars.", "This is a test with  and other chars")]
+        public void TestRemoveSpecialChars(string stringToCheck, string stringResult)
+            => Assert.Equal(stringResult, stringToCheck.RemoveSpecialChars());
+
+        [Theory]
+        [InlineData("This is a (test)", "(", ")", "This is a ")]
+        [InlineData("This (is) a (test)", "(", ")", "This  a ")]
+        [InlineData("This <testis</test> a (test)</test>", "<test", "</test>", "This  a (test)</test>")]
+        public void TestRemoveTextBetween(string stringToCheck, string removeFrom, string removeTo, string stringResult)
+            => Assert.Equal(stringResult, stringToCheck.RemoveTextBetween(removeFrom, removeTo));
+
+        [Theory]
+        [InlineData("This IS a tesT!", "This Is A Test!")]
+        public void TestToTitleCase(string stringToCheck, string stringResult)
+            => Assert.Equal(stringResult, stringToCheck.ToTitleCase());
+
+        [Theory]
+        [InlineData("This is a über test.", "This+is+a+%c3%bcber+test.")]
+        public void TestUrlEncode(string stringToCheck, string stringResult)
+            => Assert.Equal(stringResult, stringToCheck.UrlEncode());
     }
 }
