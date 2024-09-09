de742afd80d7ea3e404675ef2e1f5c4c43fcc562
Alexander Heidn <40741234+HerrKnarz@users.noreply.github.com>
2024-09-09 18:06:12 +0200

    Reworked the menus for whitelist and quick add items to accommodate for more field types

--- Generic/MetadataUtilities/Views/SettingsView.xaml.cs
+++ Generic/MetadataUtilities/Views/SettingsView.xaml.cs
@@ -1,7 +1,21 @@
-﻿namespace MetadataUtilities.Views
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace MetadataUtilities.Views
 {
     public partial class SettingsView
     {
         public SettingsView() => InitializeComponent();
+
+        private void b_Click(object sender, RoutedEventArgs e)
+        {
+            ContextMenu contextMenu = ((Button)sender)?.ContextMenu;
+
+            if (contextMenu == null)
+                return;
+
+            contextMenu.DataContext = ((Button)sender).DataContext;
+            contextMenu.IsOpen = true;
+        }
     }
 }
