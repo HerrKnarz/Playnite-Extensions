d56adb5bf2663dda3805b34bcb1f4e04b36d7a0a
Alexander Heidn <40741234+HerrKnarz@users.noreply.github.com>
2024-10-08 22:01:46 +0200

    Fixed possible null exception in conditional actions using the description field as a condition.

--- Common/KNARZhelper/DatabaseObjectTypes/TypeDescription.cs
+++ Common/KNARZhelper/DatabaseObjectTypes/TypeDescription.cs
@@ -12,8 +12,8 @@ namespace KNARZhelper.DatabaseObjectTypes
 
         public override void EmptyFieldInGame(Game game) => API.Instance.MainView.UIDispatcher.Invoke(() => game.Description = default);
 
-        public override bool FieldInGameIsEmpty(Game game) => !game.Description.Trim().Any();
+        public override bool FieldInGameIsEmpty(Game game) => !game?.Description?.Trim().Any() ?? true;
 
-        public override bool GameContainsValue(Game game, string value) => game.Description.RegExIsMatch(value);
+        public override bool GameContainsValue(Game game, string value) => value != null && (game?.Description?.RegExIsMatch(value) ?? false);
     }
 }
