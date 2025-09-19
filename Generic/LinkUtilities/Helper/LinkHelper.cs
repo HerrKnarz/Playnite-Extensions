91d53365b3774ba293c899f91b1298ac86ef2f83
Alexander Heidn <40741234+HerrKnarz@users.noreply.github.com>
2025-09-19 18:03:00 +0200

    Added user agent for better check results

--- Generic/LinkUtilities/Helper/LinkHelper.cs
+++ Generic/LinkUtilities/Helper/LinkHelper.cs
@@ -205,7 +205,8 @@ namespace LinkUtilities.Helper
                 var web = new HtmlWeb
                 {
                     UseCookies = true,
-                    PreRequest = OnPreRequest
+                    PreRequest = OnPreRequest,
+                    UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
                 };
 
                 var document = web.Load(url);
