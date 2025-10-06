d72ce9fea4f099648a5b081c83186d6c0f08a33c
Alexander Heidn <40741234+HerrKnarz@users.noreply.github.com>
2025-10-06 12:57:50 +0200

    Updated Playnite SDK version and added a very early concept of a new screenshot utilities addon

---
+++ Generic/ScreenshotUtilities/ScreenshotUtilities.cs
@@ -0,0 +1,128 @@
+﻿using Playnite.SDK;
+using Playnite.SDK.Events;
+using Playnite.SDK.Models;
+using Playnite.SDK.Plugins;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Controls;
+
+namespace ScreenshotUtilities
+{
+    public class ScreenshotUtilities : GenericPlugin
+    {
+        private ScreenshotUtilitiesSettingsViewModel Settings { get; set; }
+
+        public override Guid Id { get; } = Guid.Parse("485d682f-73e9-4d54-b16f-b8dd49e88f90");
+
+        public ScreenshotUtilities(IPlayniteAPI api) : base(api)
+        {
+            Settings = new ScreenshotUtilitiesSettingsViewModel(this);
+            Properties = new GenericPluginProperties
+            {
+                HasSettings = true
+            };
+        }
+
+        public void ShowScreenshots(Game game)
+        {
+            if (game == null)
+            {
+                PlayniteApi.Dialogs.ShowMessage("No game selected");
+                return;
+            }
+
+            var path = Path.Combine(GetPluginUserDataPath(), game.Id.ToString());
+
+            if (!Directory.Exists(path))
+            {
+                PlayniteApi.Dialogs.ShowMessage("No screenshots found for this game");
+                return;
+            }
+
+            var ext = new List<string> { "jpg", "jpeg", "gif", "png", "webp" };
+            var files = Directory
+                .EnumerateFiles(path, "*.*", SearchOption.AllDirectories)
+                .Where(file => ext.Contains(Path.GetExtension(file).TrimStart('.').ToLowerInvariant()));
+
+            var screenshots = new List<ImageFileOption>(files.Select(file => new ImageFileOption
+            {
+                Path = file,
+                Name = Path.GetFileNameWithoutExtension(file)
+            }));
+
+            API.Instance.Dialogs.ChooseImageFile(screenshots, "Screenshots", 300, 300);
+
+            return;
+        }
+
+        public override IEnumerable<GameMenuItem> GetGameMenuItems(GetGameMenuItemsArgs args)
+        {
+            var menuSection = ResourceProvider.GetString("LOCLinkUtilitiesName");
+            var menuAddLinks = ResourceProvider.GetString("LOCLinkUtilitiesMenuAddLinkTo");
+            var menuSearchLinks = ResourceProvider.GetString("LOCLinkUtilitiesMenuSearchLinkTo");
+            var menuBrowserSearchLinks = ResourceProvider.GetString("LOCLinkUtilitiesMenuBrowserSearchLinkTo");
+
+            var menuItems = new List<GameMenuItem>();
+
+            menuItems.AddRange(new List<GameMenuItem>
+            {
+                // Adds the "All configured websites" item to the "add link to" sub menu.
+                new GameMenuItem
+                {
+                    Description = "Show screenshots",
+                    MenuSection = "Screenshot Utilities",
+                    //Icon = "luAddIcon",
+                    Action = a => ShowScreenshots(args.Games.FirstOrDefault())
+                }
+            });
+
+            return menuItems;
+        }
+
+        public override void OnGameInstalled(OnGameInstalledEventArgs args)
+        {
+            // Add code to be executed when game is finished installing.
+        }
+
+        public override void OnGameStarted(OnGameStartedEventArgs args)
+        {
+            // Add code to be executed when game is started running.
+        }
+
+        public override void OnGameStarting(OnGameStartingEventArgs args)
+        {
+            // Add code to be executed when game is preparing to be started.
+        }
+
+        public override void OnGameStopped(OnGameStoppedEventArgs args)
+        {
+            // Add code to be executed when game is preparing to be started.
+        }
+
+        public override void OnGameUninstalled(OnGameUninstalledEventArgs args)
+        {
+            // Add code to be executed when game is uninstalled.
+        }
+
+        public override void OnApplicationStarted(OnApplicationStartedEventArgs args)
+        {
+            // Add code to be executed when Playnite is initialized.
+        }
+
+        public override void OnApplicationStopped(OnApplicationStoppedEventArgs args)
+        {
+            // Add code to be executed when Playnite is shutting down.
+        }
+
+        public override void OnLibraryUpdated(OnLibraryUpdatedEventArgs args)
+        {
+            // Add code to be executed when library is updated.
+        }
+
+        public override ISettings GetSettings(bool firstRunSettings) => Settings;
+
+        public override UserControl GetSettingsView(bool firstRunSettings) => new ScreenshotUtilitiesSettingsView();
+    }
+}
