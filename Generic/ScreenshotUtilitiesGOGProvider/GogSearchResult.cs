508c0b5d76464a1a6da15f947db3c2543b7449ad
Alexander Heidn <40741234+HerrKnarz@users.noreply.github.com>
2025-11-16 14:10:57 +0100

    Added search functionality

---
+++ Generic/ScreenshotUtilitiesGOGProvider/GogSearchResult.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+// Contains all the classes needed to deserialize the JSON fetched from the gog catalog URL.
+namespace ScreenshotUtilitiesGOGProvider
+{
+    public class GogSearchResult
+    {
+        [JsonProperty("products")]
+        public List<Product> Products;
+    }
+
+    public class Product
+    {
+        [JsonProperty("id")]
+        public string Id;
+
+        [JsonProperty("releaseDate")]
+        public string ReleaseDate;
+
+        [JsonProperty("title")]
+        public string Title;
+    }
+}
--- Generic/ScreenshotUtilitiesGOGProvider/ScreenshotUtilitiesGOGProvider.cs
+++ Generic/ScreenshotUtilitiesGOGProvider/ScreenshotUtilitiesGOGProvider.cs
@@ -2,6 +2,7 @@
 using KNARZhelper.ScreenshotsCommon;
 using KNARZhelper.ScreenshotsCommon.Models;
 using Playnite.SDK;
+using Playnite.SDK.Data;
 using Playnite.SDK.Events;
 using Playnite.SDK.Models;
 using Playnite.SDK.Plugins;
@@ -9,7 +10,9 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
+using System.Web.UI.WebControls;
 using System.Windows.Controls;
 
 namespace ScreenshotUtilitiesGOGProvider
@@ -18,6 +21,10 @@ namespace ScreenshotUtilitiesGOGProvider
     {
         public static Guid GogId = Guid.Parse("aebe8b7c-6dc3-4a66-af31-e7375c6b5e9e");
         public static Guid GogOssId = Guid.Parse("03689811-3F33-4DFB-A121-2EE168FB9A5C");
+
+        private readonly string _searchUrl = "https://catalog.gog.com/v1/catalog?limit=100&locale=en&order=desc:score&page=1&productType=in:game,pack&query=like:";
+        private Game _game;
+        private ScreenshotGroup _screenshotGroup;
 
         public ScreenshotUtilitiesGOGProvider(IPlayniteAPI api) : base(api)
         {
@@ -31,85 +38,41 @@ namespace ScreenshotUtilitiesGOGProvider
         public override Guid Id { get; } = Guid.Parse("228c70e8-7c89-46fc-b2c8-6e97966d01a4");
         public string Name { get; set; } = "GOG";
         public bool SupportsAutomaticScreenshots { get; set; } = true;
-        public bool SupportsScreenshotSearch { get; set; } = false;
+        public bool SupportsScreenshotSearch { get; set; } = true;
         private ScreenshotUtilitiesGOGProviderSettingsViewModel settings { get; set; }
 
         public async Task<bool> CleanUpAsync(Game game) => await ScreenshotHelper.DeleteOrphanedJsonFiles(game.Id, Id);
 
         public async Task<bool> GetScreenshotsAsync(Game game, int daysSinceLastUpdate, bool forceUpdate)
         {
-            try
-            {
-                if (!ScreenshotHelper.IsScreenshotUtilitiesInstalled || game == null)
-                {
-                    return false;
-                }
-
-                if (game.PluginId != GogId && game.PluginId != GogOssId)
-                {
-                    return false;
-                }
-
-                var gogId = game.GameId;
-
-                if (string.IsNullOrEmpty(gogId))
-                {
-                    return false;
-                }
-
-                var fileName = ScreenshotHelper.GenerateFileName(game.Id, Id, Id);
-
-                var screenshotGroup = ScreenshotGroup.CreateFromFile(new FileInfo(fileName))
-                    ?? new ScreenshotGroup(Name, Id)
-                    {
-                        Provider = new ScreenshotProvider(Name, Id),
-                        Screenshots = new RangeObservableCollection<KNARZhelper.ScreenshotsCommon.Models.Screenshot>()
-                    };
-
-                if (!forceUpdate
-                    && screenshotGroup.LastUpdate != null
-                    && (screenshotGroup.LastUpdate > DateTime.Now.AddDays(daysSinceLastUpdate * -1)))
-                {
-                    return false;
-                }
-
-                var apiUrl = $"https://api.gog.com/products/{gogId}?expand=screenshots";
-
-                var result = await ApiHelper.GetJsonFromApiAsync<GogApiResult>(apiUrl, Name);
-
-                var updated = false;
-
-                if (!(result is null) && !(result.Screenshots is null) && (result.Screenshots?.Count) != 0)
-                {
-                    foreach (var screenshot in result.Screenshots.Where(s => !screenshotGroup.Screenshots.Any(es => es.Path.Contains(s.ImageId))))
-                    {
-                        var imageUrl = screenshot.FormattedImages.Where(fi => fi.FormatterName.Equals("ggvgl")).FirstOrDefault()?.ImageUrl ?? null;
-                        var thumbUrl = screenshot.FormattedImages.Where(fi => fi.FormatterName.Equals("ggvgm")).FirstOrDefault()?.ImageUrl ?? null;
-
-                        screenshotGroup.Screenshots.Add(new KNARZhelper.ScreenshotsCommon.Models.Screenshot(imageUrl)
-                        {
-                            ThumbnailPath = thumbUrl,
-                            SortOrder = result.Screenshots.IndexOf(screenshot)
-                        });
-                    }
-
-                    updated = true;
-                }
-
-                ScreenshotHelper.SaveScreenshotGroupJson(game, screenshotGroup);
-
-                return updated;
-            }
-            catch (Exception ex)
-            {
-                Log.Error(ex, $"Error fetching screenshots for {game.Name}");
-                return false;
-            }
+            return await FetchScreenshotsAsync(game, daysSinceLastUpdate, forceUpdate);
         }
 
-        public string GetScreenshotSearchResult(Game game, string searchTerm) => throw new NotImplementedException();
+        public string GetScreenshotSearchResult(Game game, string searchTerm)
+        {
+            var gogSearchResult = ApiHelper.GetJsonFromApi<GogSearchResult>($"{_searchUrl}{searchTerm.RemoveDiacritics().UrlEncode()}", Name);
 
-        public Task<bool> GetScreenshotsManualAsync(Game game, string gameIdentifier) => throw new NotImplementedException();
+            var searchResults = new List<GenericItemOption>();
+
+            if (!gogSearchResult?.Products?.Any() ?? true)
+            {
+                return null;
+            }
+
+            var result = new List<ScreenshotSearchResult>(gogSearchResult.Products.Select(product => new ScreenshotSearchResult
+            {
+                Name = product.Title,
+                Description = $"{product.ReleaseDate} -  ID {product.Id}",
+                Identifier = product.Id
+            }));
+
+            return Serialization.ToJson(result);
+        }
+
+        public async Task<bool> GetScreenshotsManualAsync(Game game, string gameIdentifier)
+        {
+            return await FetchScreenshotsAsync(game, 0, true, gameIdentifier);
+        }
 
         public override ISettings GetSettings(bool firstRunSettings) => settings;
 
@@ -126,5 +89,112 @@ namespace ScreenshotUtilitiesGOGProvider
         }
 
         //new ScreenshotUtilitiesGOGProviderSettingsView();
+
+        private async Task<bool> FetchScreenshotsAsync(Game game, int daysSinceLastUpdate, bool forceUpdate, string gogId = default)
+        {
+            try
+            {
+                // return when the main addon isn't installed.
+                if (!ScreenshotHelper.IsScreenshotUtilitiesInstalled || game == null)
+                {
+                    return false;
+                }
+
+                // set game and load the file.
+                _game = game;
+
+                var fileExists = false;
+
+                (fileExists, _screenshotGroup) = ScreenshotHelper.LoadGroup(_game, Name, Id);
+
+                // return if we don't want to force an update and the last update was inside the
+                // days configured.
+                if (!forceUpdate
+                    && _screenshotGroup.LastUpdate != null
+                    && (_screenshotGroup.LastUpdate > DateTime.Now.AddDays(daysSinceLastUpdate * -1)))
+                {
+                    return false;
+                }
+
+                // Return if a game was searched and it's the one we already have.
+                if (gogId != default && gogId.Equals(_screenshotGroup.GameIdentifier))
+                {
+                    return false;
+                }
+
+                // Get the right name to search for.
+                var searchName = GetGogId(gogId);
+
+                if (string.IsNullOrEmpty(searchName))
+                {
+                    return false;
+                }
+
+                // We need to reset the file if we got a new gogId from the method call and it's not
+                // the same we already got.
+                if (!fileExists || (gogId != default && !searchName.Equals(_screenshotGroup.GameIdentifier)))
+                {
+                    _screenshotGroup.GameIdentifier = searchName;
+
+                    _screenshotGroup.Screenshots.Clear();
+                }
+
+                var updated = await LoadScreenshotsFromSourceAsync();
+
+                ScreenshotHelper.SaveScreenshotGroupJson(game, _screenshotGroup);
+
+                return updated;
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, $"Error fetching screenshots for {game.Name}");
+                return false;
+            }
+        }
+
+        private string GetGogId(string gogId = default)
+        {
+            var searchGogId = gogId;
+
+            if (searchGogId == default)
+            {
+                searchGogId = _screenshotGroup.GameIdentifier;
+            }
+
+            if ((searchGogId == default) && _game.PluginId.IsOneOf(GogId, GogOssId))
+            {
+                searchGogId = _game.GameId;
+            }
+
+            return searchGogId;
+        }
+
+        private async Task<bool> LoadScreenshotsFromSourceAsync()
+        {
+            var apiUrl = $"https://api.gog.com/products/{_screenshotGroup.GameIdentifier}?expand=screenshots";
+
+            var result = await ApiHelper.GetJsonFromApiAsync<GogApiResult>(apiUrl, Name);
+
+            var updated = false;
+
+            if (!(result is null) && !(result.Screenshots is null) && (result.Screenshots?.Count) != 0)
+            {
+                foreach (var screenshot in result.Screenshots.Where(s => !_screenshotGroup.Screenshots.Any(es => es.Path.Contains(s.ImageId))))
+                {
+                    var imageUrl = screenshot.FormattedImages.Where(fi => fi.FormatterName.Equals("ggvgl")).FirstOrDefault()?.ImageUrl ?? null;
+                    var thumbUrl = screenshot.FormattedImages.Where(fi => fi.FormatterName.Equals("ggvgm")).FirstOrDefault()?.ImageUrl ?? null;
+
+                    _screenshotGroup.Screenshots.Add(new KNARZhelper.ScreenshotsCommon.Models.Screenshot(imageUrl)
+                    {
+                        ThumbnailPath = thumbUrl,
+                        SortOrder = result.Screenshots.IndexOf(screenshot)
+                    });
+                }
+
+                updated = true;
+            }
+
+            return updated;
+        }
     }
 }
