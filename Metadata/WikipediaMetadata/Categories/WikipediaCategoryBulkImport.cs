89a98a0b751f6f3dfcaefdb4f9625b145d6c4f63
Jeshibu <jeshibu@gmail.com>
2026-01-16 23:12:45 +0100

    Merge WikipediaHelper classes

--- Metadata/WikipediaMetadata/Categories/WikipediaCategoryBulkImport.cs
+++ Metadata/WikipediaMetadata/Categories/WikipediaCategoryBulkImport.cs
@@ -4,7 +4,6 @@ using PlayniteExtensions.Common;
 using PlayniteExtensions.Metadata.Common;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text.RegularExpressions;
 using System.Windows;
 using WikipediaMetadata.Categories.Models;
 
@@ -121,31 +120,6 @@ public class WikipediaCategoryBulkImport : BulkGamePropertyAssigner<WikipediaSea
         }
 
         return false;
-    }
-}
-
-public static class WikipediaHelper
-{
-    private static readonly Regex TitleParenthesesRegex = new(@"^(?<title>.+) \((?<parenContents>.+)\)$", RegexOptions.Compiled);
-
-    extension(string articleName)
-    {
-        public string StripCategoryPrefix() => articleName?.Split([':'], 2).Last();
-
-        public bool IsArticleAboutGame(out string displayTitle)
-        {
-            displayTitle = articleName;
-            var match = TitleParenthesesRegex.Match(articleName);
-            if (match.Success)
-            {
-                var parenContents = match.Groups["parenContents"].Value;
-                if (parenContents.Contains("comic") || parenContents.Contains("soundtrack") || parenContents.Contains("film") || parenContents.Contains("novel"))
-                    return false;
-            }
-
-            displayTitle = match.Success ? match.Groups["title"].Value : articleName;
-            return true;
-        }
     }
 }
 
--- Metadata/WikipediaMetadata/WikipediaHelper.cs
+++ Metadata/WikipediaMetadata/WikipediaHelper.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace WikipediaMetadata;
 
@@ -11,4 +12,26 @@ internal static class WikipediaHelper
     /// <returns>Url of the image</returns>
     internal static string GetImageUrl(string key) =>
         WikipediaApiCaller.GetImage(key)?.Query?.Pages.FirstOrDefault()?.Original?.Source;
+
+    private static readonly Regex TitleParenthesesRegex = new(@"^(?<title>.+) \((?<parenContents>.+)\)$", RegexOptions.Compiled);
+
+    extension(string articleName)
+    {
+        public string StripCategoryPrefix() => articleName?.Split([':'], 2).Last();
+
+        public bool IsArticleAboutGame(out string displayTitle)
+        {
+            displayTitle = articleName;
+            var match = TitleParenthesesRegex.Match(articleName);
+            if (match.Success)
+            {
+                var parenContents = match.Groups["parenContents"].Value;
+                if (parenContents.Contains("comic") || parenContents.Contains("soundtrack") || parenContents.Contains("film") || parenContents.Contains("novel"))
+                    return false;
+            }
+
+            displayTitle = match.Success ? match.Groups["title"].Value : articleName;
+            return true;
+        }
+    }
 }
