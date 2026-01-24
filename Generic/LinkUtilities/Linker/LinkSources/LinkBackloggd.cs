031185a8d56a61913a6b8a23d65e6e4823ded692
Alexander Heidn <40741234+HerrKnarz@users.noreply.github.com>
2026-01-24 16:41:16 +0100

    Fixed and improved Backloggd search results after changes in the HTML on their end.

--- Generic/LinkUtilities/Linker/LinkSources/LinkBackloggd.cs
+++ Generic/LinkUtilities/Linker/LinkSources/LinkBackloggd.cs
@@ -7,12 +7,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+
 // ReSharper disable CommentTypo
 
 namespace LinkUtilities.Linker.LinkSources
 {
     /// <summary>
-    ///     Adds a link to Backloggd.
+    /// Adds a link to Backloggd.
     /// </summary>
     internal class LinkBackloggd : BaseClasses.Linker
     {
@@ -22,11 +23,13 @@ namespace LinkUtilities.Linker.LinkSources
         public override string SearchUrl => _websiteUrl + "/search/games/";
         public override UrlLoadMethod UrlLoadMethod => UrlLoadMethod.Load;
 
-        // Since Backloggd always returns the status code OK and the same url, even if that leads to a non-existing game, we also check if the title isn't the one
-        // of that generic page. Funny enough it says 404 but doesn't return that status code...
+        // Since Backloggd always returns the status code OK and the same url, even if that leads to
+        // a non-existing game, we also check if the title isn't the one of that generic page. Funny
+        // enough it says 404 but doesn't return that status code...
         public override string WrongTitle => "404 Not Found";
 
-        // Backloggd Links need the game name in lowercase without special characters and hyphens instead of white spaces.
+        // Backloggd Links need the game name in lowercase without special characters and hyphens
+        // instead of white spaces.
         public override string GetGamePath(Game game, string gameName = null)
             => (gameName ?? game.Name).RemoveSpecialChars()
                 .CollapseWhitespaces()
@@ -48,12 +51,48 @@ namespace LinkUtilities.Linker.LinkSources
 
                 if (htmlNodes?.Any() ?? false)
                 {
-                    return new List<GenericItemOption>(htmlNodes.Select(n => new SearchResult
+                    var resultList = new List<GenericItemOption>();
+
+                    foreach (var n in htmlNodes)
                     {
-                        Name = WebUtility.HtmlDecode(n.SelectSingleNode(".//div[contains(@class, 'game-name')]/a/h3").InnerText),
-                        Url = $"{_websiteUrl}{n.SelectSingleNode(".//div[contains(@class, 'game-name')]/a").GetAttributeValue("href", "")}",
-                        Description = WebUtility.HtmlDecode(n.SelectSingleNode(".//h1[contains(@class, 'game-date')]").InnerText)
-                    }));
+                        var nameHtml = n.SelectSingleNode(".//div[contains(@class, 'game-name')]/a");
+
+                        if (nameHtml != null)
+                        {
+                            var platformHtml = n.SelectSingleNode(".//div[contains(@class, 'search-result-platforms')]");
+                            var description = nameHtml.SelectSingleNode(".//span[contains(@class, 'subtitle-text')]")?.InnerText ?? string.Empty;
+
+                            if (platformHtml != null)
+                            {
+                                var yearHtml = nameHtml.SelectSingleNode(".//span[contains(@class, 'subtitle-text')]");
+
+                                if (yearHtml != null)
+                                {
+                                    description = WebUtility.HtmlDecode(yearHtml.InnerText) + " - ";
+                                }
+
+                                var typeHtml = platformHtml.SelectSingleNode(".//p");
+
+                                if (typeHtml != null)
+                                {
+                                    description += WebUtility.HtmlDecode(typeHtml.InnerText) + " - ";
+                                }
+
+                                description += ResourceProvider.GetString("LOCPlatformsTitle") + ": ";
+
+                                description += string.Join(", ", platformHtml.SelectNodes(".//span/p").Select(p => p.InnerText));
+                            }
+
+                            resultList.Add(new SearchResult
+                            {
+                                Name = WebUtility.HtmlDecode(nameHtml.SelectSingleNode(".//h3").GetDirectInnerText()),
+                                Url = $"{_websiteUrl}{nameHtml.GetAttributeValue("href", "")}",
+                                Description = description
+                            });
+                        }
+                    }
+
+                    return resultList;
                 }
             }
             catch (Exception ex)
